3b96c757c32a33737cf5270012800d4fb3103ea6
KevinPham <kevinpham15@gmail.com>
2017-10-31 14:19:39 -0700

    initial commit

---
+++ Heads_Tails.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+var flip = [
+    0: "head",
+    1: "tails"
+]
+
+var toss = Int(arc4random_uniform(UInt32()))
+
+func tossCoin(){
+    print("Tossing a Coin")
+}
+
+
